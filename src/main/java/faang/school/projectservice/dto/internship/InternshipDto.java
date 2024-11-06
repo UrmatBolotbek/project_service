3425f473710ad96f73dec54e039e22bc2cff3377
Roma Nifontov <romannif@mail.ru>
2024-11-06 16:25:05 +0500

    промежуточный коммит

--- src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
+++ src/main/java/faang/school/projectservice/dto/internship/InternshipDto.java
@@ -1,17 +1,18 @@
 package faang.school.projectservice.dto.internship;
 
+import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 public class InternshipDto {
 
     private Long id;
-    private final Long projectId;
+    private Long projectId;
     private Long mentorId;
-    private List<Long> interns;
+    private List<Long> internsId;
     private String name;
     private String description;
 
--- src/main/java/faang/school/projectservice/mapper/internship/InternshipMapper.java
+++ src/main/java/faang/school/projectservice/mapper/internship/InternshipMapper.java
@@ -5,16 +5,24 @@ import faang.school.projectservice.model.Internship;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface InternshipMapper {
 
     @Mapping(target = "project", ignore = true)
     @Mapping(target = "mentorId", ignore = true)
+    @Mapping(target = "interns", ignore = true)
     Internship toInternship(InternshipDto internshipDto);
 
     @Mapping(source = "project.id", target = "projectId")
     @Mapping(source = "mentorId.id", target = "mentorId")
+    @Mapping(source = "interns", target = "internsId", qualifiedByName = "toInternshipIds")
     InternshipDto toInternshipDto(Internship internship);
+
+    default List<Long> toInternshipIds(List<Internship> internships) {
+      return internships.stream().map(Internship::getId).toList();
+    }
 
 
 }
