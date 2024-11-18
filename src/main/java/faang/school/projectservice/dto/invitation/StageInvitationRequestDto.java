29e7abff6d1b3f2aa253f19a49f992b0ad4200ad
zlomilk95 <kenobi_95@mail.ru>
2024-11-18 23:11:34 +0300

    add field status in request Dto for invitation

--- src/main/java/faang/school/projectservice/dto/invitation/StageInvitationRequestDto.java
+++ src/main/java/faang/school/projectservice/dto/invitation/StageInvitationRequestDto.java
@@ -1,5 +1,6 @@
 package faang.school.projectservice.dto.invitation;
 
+import faang.school.projectservice.model.stage_invitation.StageInvitationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,5 @@ public class StageInvitationRequestDto {
     private Long invitedId;
     private Long authorId;
     private String description;
+    private StageInvitationStatus status;
 }
